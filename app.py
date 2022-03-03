74a0b0cdc28838162bcc066d9c872663067c5350
mleitgeb <mleitgeb@iu.edu>
2022-03-03 18:40:50 -0500

    Dino Site

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import csv
 app = Flask(__name__)
+
+with open('dinosaurs.csv', 'r') as csvfile:
+    data = csv.DictReader(csvfile)
+    dinosaurs = {row['slug']:{'name':row['name'],'description':row['description'],'image':row['image'],'image-credit':row['image-credit'],'source-url':row['source-url'],'source-credit':row['source-credit']} for row in data}
 
 @app.route('/')
 
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', dinosaurs=dinosaurs)
+
+# def hello_world():
+#     return 'Hello World!'
